bb22f1244ed6e762102629339bc1d50503e66312
Jonathan Moreschi <j.moreschi@outlook.fr>
2025-07-01 16:30:42 +0200

    Protect role in user register

--- src/database/users.ts
+++ src/database/users.ts
@@ -6,13 +6,18 @@ const prisma = new PrismaClient()
 
 export async function registerUser(req:UserRegister) {
 
-
+  const isFirstUser = await prisma.user.count() === 0;
+  if (isFirstUser && req.role !== Role.ADMIN) {
+    req.role = Role.ADMIN; // If it's the first user, set role to ADMIN
+  } else {
+    req.role = Role.USER; // Default to USER if not specified
+  }
   const user = await prisma.user.create({
     data: {
       name: req.name,
       email: req.email,
       password: hashPassword(req.password),
-      role: req.role || Role.USER,
+      role: req.role,
     },
   })
 
