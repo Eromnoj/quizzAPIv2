5ad8a7666d7e18057e876ceacd5abc2b1d5b198f
Jonathan Moreschi <j.moreschi@outlook.fr>
2025-10-17 15:33:49 +0200

    Doc: Add documentation to seed.ts, commenting the parts necessary for first db seeding

--- src/database/feed/default/seed.ts
+++ src/database/feed/default/seed.ts
@@ -7,20 +7,75 @@ const prisma = new PrismaClient();
 async function seed() {
   const basePath = path.resolve(__dirname);
 
-  const categories = JSON.parse(await fs.readFile(path.join(basePath, 'categories.json'), 'utf-8'));
+  /* 
+  ** "categories.json" entries must respect this template 
+  ** 
+  [
+      {
+        "name": "musique",
+        "slug": "musique"
+      },
+      {
+        "name": "TV et cinéma",
+        "slug": "tv_cinema"
+      },
+      {
+        "name": "sport",
+        "slug": "sport"
+      },
+  ]
+  **
+  **
+  */
+
+  // ** For first seeding uncomment this part to seed your categories. 
+  // const categories = JSON.parse(await fs.readFile(path.join(basePath, 'categories.json'), 'utf-8'));
+  
+  /*
+    ** "questions.json" entries must respect this template, the category must be one the slugs you chose ealier
+  ** 
+  [
+    {
+        "question": "En quelle année les jeux FIFA Football ont-ils commencé à être édités ?",
+        "answer": "1993",
+        "category": "jeux_videos",
+        "difficulty": "facile",
+        "badAnswers": [
+            "1994",
+            "1998",
+            "2000"
+        ]
+    },
+    {
+        "question": "Que signifie chez les Ch’tis le mot babache ?",
+        "answer": "Une personne idiote",
+        "category": "culture_generale",
+        "difficulty": "normal",
+        "badAnswers": [
+            "Une personne bavarde",
+            "Une personne peureuse",
+            "Une personne gourmande"
+        ]
+    }
+  ]
+  **
+  **
+  */
   const questions = JSON.parse(await fs.readFile(path.join(basePath, 'questions.json'), 'utf-8'));
 
-  console.log('Seeding database...', questions);
-  const isEmptyCategory = await prisma.category.findMany();
-  if (isEmptyCategory.length === 0) {
-    await prisma.category.createMany({ data: categories });
-    console.log('✅ Categories seeded');
-  } else {
-    console.log('⚠️ Categories already seeded');
-  }
 
-  const isEmptyQuestion = await prisma.quiz.findMany();
-  if (isEmptyQuestion.length === 0) {
+  // ** For first seeding uncomment this part to seed your categories.
+  // const isEmptyCategory = await prisma.category.findMany();
+  // if (isEmptyCategory.length === 0) {
+  //   await prisma.category.createMany({ data: categories });
+  //   console.log('✅ Categories seeded');
+  // } else {
+  //   console.log('⚠️ Categories already seeded');
+  // }
+
+  // ** For first seeding uncomment this part to seed your questions.
+  // const isEmptyQuestion = await prisma.quiz.findMany();
+  // if (isEmptyQuestion.length === 0) {
 
     let formattedQuestions = await Promise.all(questions.map(async (question: any) => {
       const category = await prisma.category.findFirst({
@@ -41,17 +96,18 @@ async function seed() {
         badAnswer3: question.badAnswers[2],
         categoryId: question.categoryId,
         difficulty: question.difficulty,
-        pending: false, // Assuming all questions are not pending by default
+        pending: true, // Pass it to false if you your questions to be immediatly available
       };
     }));
 
 
     await prisma.quiz.createMany({ data: formattedQuestions });
     console.log('✅ Questions seeded');
-  } else {
-    console.log('⚠️ Questions already seeded');
-  }
- 
+    // ** For first seeding uncomment this part to seed your questions.
+  // } else {
+  //   console.log('⚠️ Questions already seeded');
+  // }
+
 
   console.log('✅ Seed completed');
 }
@@ -62,3 +118,5 @@ seed()
     process.exit(1);
   })
   .finally(() => prisma.$disconnect());
+
+
